74a8cdee7f370ae3bc8eb85ea67ac33221969a5a
lxt2 <rznqsp8ffyeprn2l@gmail.com>
2016-09-26 21:23:12 +1000

    Iterate on deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,40 +1,55 @@
 #!/bin/bash
 set -eu -o pipefail
 
-DOCKER_TAG=${DOCKER_TAG:-$IMAGE:$COMMIT}
+DOCKER_IMAGE=${DOCKER_IMAGE:-cardigann/cardigann}
+DOCKER_TAG=${DOCKER_TAG:-$DOCKER_IMAGE:$COMMIT}
+VERSION=$(git describe --tags --candidates=1 --dirty)
 
-# docker: $(BIN)-linux-amd64 cacert.pem
-# wget -N https://curl.haxx.se/ca/cacert.pem
+download_cacert() {
+  wget -N https://curl.haxx.se/ca/cacert.pem
+}
 
 docker_build() {
   touch server/static.go
   make cardigann-linux-amd64
   file cardigann-linux-amd64
-  docker build -t
+  download_cacert
+  docker build -t "${DOCKER_TAG}" .
   docker run --rm -it "${DOCKER_TAG}" version
-  docker tag "${DOCKER_TAG}" "${IMAGE}:latest"
+  docker tag "${DOCKER_TAG}" "${DOCKER_IMAGE}:latest"
 }
 
 docker_login() {
   docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 }
 
-echo "Building docker image for ${IMAGE}:${COMMIT}"
+download_cacert() {
+  wget -N https://curl.haxx.se/ca/cacert.pem
+}
+
+download_equinox() {
+  wget -N https://bin.equinox.io/c/mBWdkfai63v/release-tool-stable-linux-amd64.tgz
+  tar -vxf release-tool-stable-linux-amd64.tgz
+}
+
+equinox_release() {
+  download_equinox
+  ./equinox release \
+    --version="$VERSION" \
+    --config ./equinox.yml \
+    --channel edge \
+    -- -ldflags="-X main.Version=$VERSION -s -w" \
+    github.com/cardigann/cardigann
+}
+
+echo "Building docker image ${DOCKER_TAG}"
 docker_build
+docker_login
 
-# Build the docker image
+echo "Releasing to equinox.io"
+equinox_release
 
-
-# Download and unpack the most recent Equinox release tool
-# wget https://bin.equinox.io/c/mBWdkfai63v/release-tool-stable-linux-amd64.tgz
-# tar -vxf release-tool-stable-linux-amd64.tgz
-
-# # TODO: Replace app_xxx with correct application ID
-# ./equinox release \
-# 	--version="$(VERSION)" \
-# 	--config ./equinox.yml \
-# 	--channel $(EQUINOX_CHANNEL) \
-# 	-- -ldflags="$(FLAGS)" $(PREFIX)
+echo "Pushing docker image ${DOCKER_TAG}"
+docker push ${DOCKER_TAG}
 
 
-# docker push ${IMAGE}
